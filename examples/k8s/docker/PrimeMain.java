34f5a74dbe9a8e043de494e5c62b8e4e982d3249
Tom Granot <8835035+TomGranot@users.noreply.github.com>
2022-03-13 14:38:46 +0200

    Lint PrimeMain

--- examples/k8s/docker/PrimeMain.java
+++ examples/k8s/docker/PrimeMain.java
@@ -1,31 +1,24 @@
-
 public class PrimeMain {
     public static int cnt = 0;
 
-	private static boolean isPrime(int num) 
-	{
-		if (num == 2)
-			return true;
-		if (num < 2 || num % 2 == 0) 
-			return false;
-		for (int i = 3; i * i <= num; i += 2)
-			if (num % i == 0) 
-				return false;
-		return true;
-	}
+    private static boolean isPrime(int num) {
+        if (num == 2)
+            return true;
+        if (num < 2 || num % 2 == 0)
+            return false;
+        for (int i = 3; i * i <= num; i += 2)
+            if (num % i == 0)
+                return false;
+        return true;
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         for (int i = 2; i < Math.pow(10, 9); ++i) {
             if (isPrime(i)) {
-            	cnt++;
+                cnt++;
             }
         }
         System.out.println("Total number of primes: " + cnt);
-	}
+    }
 
 }
-
-
-
-
-
